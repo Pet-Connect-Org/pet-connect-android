97efef4279a6cb8a69a0d7e7da36b9db0c08f7e9
Vu Thanh Xuan <vuthanhxuan333@gmail.com>
2024-03-21 22:03:17 +0700

    00

--- app/src/main/java/com/example/petconnect/services/auth/LoginRequest.java
+++ app/src/main/java/com/example/petconnect/services/auth/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.example.petconnect.services.auth;
 
 public class LoginRequest {
+
     private String email;
     private String password;
     public LoginRequest(String email, String password) {

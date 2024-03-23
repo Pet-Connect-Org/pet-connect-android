f2b3fe52387bed84f066e907fba4dceb5cafea24
buithuyngoc <buithuyngoc1@gmail.com>
2024-03-23 17:36:37 +0700

    feat: update avatar

--- app/src/main/java/com/example/petconnect/CustomAvatar.java
+++ app/src/main/java/com/example/petconnect/CustomAvatar.java
@@ -11,23 +11,34 @@ import com.example.petconnect.models.ExtendedAccount;
 
 public class CustomAvatar extends LinearLayout {
     Button button;
+
     public CustomAvatar(Context context) {
         super(context);
         initializeViews(context, null);
     }
+
     public CustomAvatar(Context context, AttributeSet attrs) {
         super(context, attrs);
         initializeViews(context, attrs);
     }
+
     private void initializeViews(Context context, AttributeSet attrs) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.custom_avatar, this);
-//        ExtendedAccount user = (new UserManager(this.getContext()).getUser());
-//        button = findViewById(R.id.avatar);
-//        this.setName(user.getUser().getName());
+
+        button = findViewById(R.id.avatar);
+
+        UserManager userManager = new UserManager(context);
+        ExtendedAccount user = userManager.getUser();
+
+        if (user != null && user.getUser() != null && user.getUser().getName() != null) {
+            setName(user.getUser().getName());
+        }
     }
 
     public void setName(String name) {
-        button.setText(name.toUpperCase().charAt(0));
+        if (button != null) {
+            button.setText(name.toUpperCase().charAt(0));
+        }
     }
 }

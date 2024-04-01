b7cd7fd7f93750a2227bd863be60066bfc5e9ebd
Vu Thanh Xuan <vuthanhxuan333@gmail.com>
2024-04-01 16:57:16 +0700

    xemlaiotp

--- app/src/main/java/com/example/petconnect/activity/OtpActivity.java
+++ app/src/main/java/com/example/petconnect/activity/OtpActivity.java
@@ -68,7 +68,6 @@ public class OtpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (isAnyTextViewNull()) {
-                    txtWrong.setVisibility(View.VISIBLE);
                     return;
                 }
 
@@ -167,7 +166,6 @@ public class OtpActivity extends AppCompatActivity {
 
 
                 txtResend.setOnClickListener(new View.OnClickListener() {
-
                     @Override
                     public void onClick(View view) {
                         if (email != null && !email.isEmpty()) {
@@ -188,7 +186,6 @@ public class OtpActivity extends AppCompatActivity {
                                 @Override
                                 public void onFailure(Call<ResendResponse> call, Throwable t) {
                                     startResendCountdown();
-
                                     Toast.makeText(OtpActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             });

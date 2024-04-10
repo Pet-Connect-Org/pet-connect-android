16f89555ca4c0f8ea03e540f731c05dd480b351f
buithuyngoc <buithuyngoc1@gmail.com>
2024-04-11 01:06:38 +0700

    update

--- app/src/main/java/com/example/petconnect/activity/ProfileActivity.java
+++ app/src/main/java/com/example/petconnect/activity/ProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.petconnect.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +9,6 @@ import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.petconnect.CustomAvatar;
 import com.example.petconnect.R;
@@ -30,7 +28,6 @@ import com.example.petconnect.services.user.UnFollowResponse;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import retrofit2.Call;
 import retrofit2.Callback;

aa4906d2d1412b282a82bde51c46bf0352fb64db
buithuyngoc <buithuyngoc1@gmail.com>
2024-04-12 03:16:50 +0700

    update model and create pet model

---
+++ app/src/main/java/com/example/petconnect/models/ExtendedPet.java
@@ -0,0 +1,13 @@
+package com.example.petconnect.models;
+
+public class ExtendedPet extends Pet{
+    PetType pet_type;
+
+    public PetType getPet_type() {
+        return pet_type;
+    }
+
+    public void setPet_type(PetType pet_type) {
+        this.pet_type = pet_type;
+    }
+}
